38aaf7b0243fb40fa2a1d96c74fb0aafa3865f36
Masato Naka <masatonaka1989@gmail.com>
2021-12-30 17:22:24 +0900

    Defind SecretMirror API

--- api/v1alpha1/secretmirror_types.go
+++ api/v1alpha1/secretmirror_types.go
@@ -20,22 +20,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SecretMirrorSpec defines the desired state of SecretMirror
 type SecretMirrorSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of SecretMirror. Edit secretmirror_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// FromNamespace is a namespace from which the target Secret is mirrored.
+	// +kubebuilder:validation:Required
+	FromNamespace string `json:"fromNamespace"`
 }
 
 // SecretMirrorStatus defines the observed state of SecretMirror
 type SecretMirrorStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
